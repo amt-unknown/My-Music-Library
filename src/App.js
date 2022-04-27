93a9fadc1639b38899fce8567b624fffd3ec8685
Andrew Thompson <amtunknown@Andrews-MacBook-Pro.local>
2022-04-27 16:47:15 -0700

    Test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 
 function App() {
   let [search, setSearch] = useState('')
-  let [message, setMessage] = useState('Search for Music! THIS IS A TEST')
+  let [message, setMessage] = useState('Search for Music!')
   let [data, setData] = useState([])
 
   const API_URL = 'https://itunes.apple.com/search?term='
